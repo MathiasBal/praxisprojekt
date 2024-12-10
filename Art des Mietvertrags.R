31f2b1e80b20c1dc1bd47c6b2f2252071d553028
LouisTrierweiler <Louis.Trierweiler@campus.lmu.de>
2024-12-10 12:31:30 +0100

    grafik Art des Mietvertrags

--- Art des Mietvertrags.R
+++ Art des Mietvertrags.R
@@ -13,8 +13,16 @@ pd <- pricedrivers %>%
   )
 
 
-#Art des Mietvertrags
+#genaue Anzahl
 g.mietverträge <- pd
+g.mietverträge_count <- g.mietverträge %>%
+  select(nmqm, ovm21, contract_type) %>%
+  filter(ovm21 < nmqm) %>%
+  group_by(contract_type) %>%
+  summarise(count = n())
+
+
+#Art des Mietvertrags
 g.mietverträge %>% 
   select(nmqm, ovm21, contract_type) %>% 
   filter(ovm21 < nmqm) %>%
@@ -22,8 +30,11 @@ g.mietverträge %>%
   geom_bar(color = "skyblue",
            fill = "skyblue",
            width = 0.7) +
+  geom_text(data = g.mietverträge_count,
+            aes(x = contract_type, y = count, label = count), 
+            size = 3.5,
+            vjust = -0.5) +
   labs(x = "Art des Mietvertrags", y = "Anzahl", title = "Anzahl der verschiedenen Mietverträge") +
   scale_y_continuous(breaks = seq(0, 1750, by= 250)) +
   theme_minimal() +
   theme(axis.text.x = element_text(size = 10))
-
