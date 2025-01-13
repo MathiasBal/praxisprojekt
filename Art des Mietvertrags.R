41a4cbf751fa134373d6b56acf07e83719647588
LouisTrierweiler <Louis.Trierweiler@campus.lmu.de>
2025-01-13 14:50:42 +0100

    grafik mietverträge

---
+++ Art des Mietvertrags.R
@@ -0,0 +1,41 @@
+#Namen der verschiedenen Mietverträge
+pd <- pricedrivers %>%
+  mutate(contract_type = factor(
+      contract_type,
+      levels = c(1, 2, 3, 4),
+      labels = c(
+        "Index",
+        "Staffel",
+        "keine Regelung",
+        "unbekannt"
+      )
+    )
+  )
+install.packages("forcats")
+library(forcats)
+
+#genaue Anzahl
+g.mietverträge <- pd
+g.mietverträge_count <- g.mietverträge %>%
+  select(net_rent_per_qm, avg_comparative_rent, contract_type) %>%
+  filter(avg_comparative_rent < net_rent_per_qm) %>%
+  group_by(contract_type) %>%
+  summarise(count = n())
+
+
+#Art des Mietvertrags
+g.mietverträge %>% 
+  select(net_rent_per_qm, avg_comparative_rent, contract_type) %>% 
+  filter(avg_comparative_rent < net_rent_per_qm) %>%
+  ggplot(aes(x = fct_infreq(contract_type))) +
+  geom_bar(color = "skyblue",
+           fill = "skyblue",
+           width = 0.7) +
+  geom_text(data = g.mietverträge_count,
+            aes(x = contract_type, y = count, label = count), 
+            size = 3.5,
+            vjust = -0.5) +
+  labs(x = "Art des Mietvertrags", y = "Anzahl", title = "Anzahl der verschiedenen Mietverträge") +
+  scale_y_continuous(breaks = seq(0, 1750, by= 250)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 10))
