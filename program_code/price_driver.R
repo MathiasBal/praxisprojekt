39bf97cc5ba8d359e3d4ed7ab3d5793691c9a21d
MathiasBal <43315288+MathiasBal@users.noreply.github.com>
2025-03-03 14:19:20 +0100

    Delete program_code directory

--- program_code/price_driver.R
+++
@@ -1,5 +0,0 @@
-# Annahme: Der DataFrame "anf_park_ws24" ist bereits im Arbeitsspeicher
-# "Preistreiber" berechnen
-
-pricedrivers <- anf.prak.ws24 %>%
-  filter(net_rent_per_qm > avg_comparative_rent)
