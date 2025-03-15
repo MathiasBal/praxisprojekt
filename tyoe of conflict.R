9348c70e7de3518b061590b6e8d22e45cf9018e0
LouisTrierweiler <Louis.Trierweiler@campus.lmu.de>
2025-03-15 23:18:28 +0100

    type of conflict (line plot)

---
+++ tyoe of conflict.R
@@ -0,0 +1,45 @@
+# load package
+library(ggplot2)
+library(dplyr)
+library(viridis)
+
+
+# translation
+event_translation <- c(
+  "Battles" = "bewaffnete Kämpfe",
+  "Explosions/Remote violence" = "Explosionen/Ferngewalt",
+  "Protests" = "Proteste",
+  "Riots" = "Randalierungen",
+  "Strategic developments" = "Strategische Entwicklungen",
+  "Violence against civilians" = "Angriffe"
+)
+
+
+# filtering
+conflict_trends <- nigeria.merged %>%
+  filter(!is.na(event_type) & event_type != "NULL") %>%
+  group_by(year = year(event_date), event_type) %>%
+  summarise(conflict_count = n(), .groups = "drop") %>%
+  mutate(event_type_de = recode(event_type, !!!event_translation))
+
+
+# line plot
+ggplot(conflict_trends, aes(x = year, y = conflict_count, color = event_type_de, group = event_type_de)) +
+  geom_line(size = 1.5, linewidth = 1.5) + 
+  scale_color_viridis_d(option = "plasma") + 
+  labs(
+    title = "Konflikttypen in Nigeria",
+    x = "Jahr",
+    y = "Anzahl der Konflikte",
+    color = "Konflikttyp"
+  ) +
+  theme_minimal() +
+  theme(
+    text = element_text(size = 14),
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+    plot.subtitle = element_text(size = 14, hjust = 0.5),
+    legend.position = "bottom",
+    legend.text = element_text(size = 12),
+    legend.box = "horizontal",
+    panel.grid.minor = element_blank()  
+  )
