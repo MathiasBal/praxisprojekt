611a64bd6e47c9041e2e5b25d65958eea077620b
MathiasBal <mathiiias.bal@gmail.com>
2025-03-16 20:50:55 +0100

    Add civilians_targeted

--- civilians_targeted.R
+++ civilians_targeted.R
@@ -1,2 +1,27 @@
-# By which group are civilians most targeted
-ggplot(nigeria.merged, aes(x=year))
+# Civilian Targeting by Event Type
+
+civilians_targeted <- nigeria.merged %>% 
+  filter(civilian_targeting == 2)
+
+nigeria.merged %>%
+  filter(event_type != "NULL") %>%
+  filter(!is.na(civilian_targeting)) %>% 
+  ggplot(
+    aes(
+      x = event_type, 
+      fill = factor(civilian_targeting)
+    )
+  ) +
+  geom_bar() +
+  scale_fill_manual(
+    values = c("gray", "red"), 
+    labels = c("No Targeting", "Civilian Targeting")
+  ) +
+  labs(
+    title = "Event Types and Civilian Targeting",
+    x = "Event Type",
+    y = "Count of Events",
+    fill = "Civilian Targeting"
+  ) +
+  theme_minimal()
+
