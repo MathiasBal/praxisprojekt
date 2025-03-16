e7332ef437cb146781fe9ebfb4794cce2ab5c778
MathiasBal <mathiiias.bal@gmail.com>
2025-03-16 19:31:45 +0100

    Filter for international sources

--- peak_years_reporting.R
+++ peak_years_reporting.R
@@ -14,3 +14,32 @@ nigeria.merged %>%
   geom_point() +
   theme_minimal()
 
+## Reports per source
+
+### Total
+
+top_sources <- nigeria.merged %>% 
+  count(source) %>% 
+  top_n(5, n) %>% 
+  pull(source)
+
+nigeria.merged %>%
+  filter(source %in% top_sources) %>% 
+  count(year, source) %>% 
+  ggplot(
+    aes(
+      x = year, 
+      y = n,
+      color = source,
+      group = source
+    )
+  ) +
+  labs(
+    title = "Top Reports Over Time by News Source", 
+    x = "Year", 
+    y = "Number of reports",
+    color = "News Source"
+  ) +
+  geom_line() +
+  geom_point() +
+  theme_minimal()
