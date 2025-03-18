cf7612413ab9aae309647c4848e5655d1026a284
FabBob <B.Faber@campus.lmu.de>
2025-03-18 13:01:58 +0100

    libraries update graph beschreibung

--- graphs code/Bevölkerungsverteilung der Akteure.R
+++ graphs code/Bevölkerungsverteilung der Akteure.R
@@ -11,8 +11,8 @@ df <- df %>% filter(!is.na(population_best))
 boxplot <- ggplot(df, aes(x = actor_group1, y = population_best)) +
   geom_boxplot(outlier.shape = NA, fill = "steelblue", alpha = 0.6) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 125000)) + 
-  labs(title = "Bevölkerungsverteilung nach Akteursgruppen",
-       x = "Akteursgruppe",
+  labs(title = "Bevölkerungsverteilung nach Akteuren",
+       x = "Akteure",
        y = "Bevölkerung am Ereignisort") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
