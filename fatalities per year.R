927d3401202b7633a6bcc36121056c859be7c998
FabBob <B.Faber@campus.lmu.de>
2025-03-18 11:45:29 +0100

    Louis graphen vollständig

---
+++ fatalities per year.R
@@ -0,0 +1,19 @@
+# fatatlities per year
+
+# count deaths by year
+fatalities_summary <- nigeria.merged %>%
+  filter(!is.na(year)) %>% 
+  group_by(year) %>% 
+  summarise(total_fatalities = sum(fatalities, na.rm = TRUE))
+
+
+# graph
+ggplot(fatalities_summary, aes(x = as.factor(year), y = total_fatalities)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = total_fatalities), vjust = -0.5, size = 2.5) +
+  labs(title = "Anzahl der Todesfälle pro Jahr in Nigeria",
+       x = "Jahr",
+       y = "Todesfälle") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
+        axis.text.x = element_text(hjust = 0.5))
