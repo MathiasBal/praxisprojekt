57c8c2329097ccd1197538ce2ab2b5c75a09af72
FabBob <B.Faber@campus.lmu.de>
2025-04-02 16:22:53 +0200

    print delete und strich zu punkt

--- graphs code/actors_pop_distr.R
+++ graphs code/actors_pop_distr.R
@@ -5,7 +5,7 @@ dataframe.actors.population <- nigeria.merged
 dataframe.actors.population <- dataframe.actors.population %>% filter(!is.na(population_best))
 
 
-boxplot.actors.population <- ggplot(dataframe.actors.population, aes(x = actor_group1, y = population_best)) +
+ggplot(dataframe.actors.population, aes(x = actor_group1, y = population_best)) +
   geom_boxplot(outlier.shape = NA, fill = "#779776", alpha = 0.6) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 125000)) + 
   labs(title = "Zusammenhang zwischen Akteursgruppen und der Bevölkerungsanzahl am Ereignisort",
@@ -13,5 +13,3 @@ boxplot.actors.population <- ggplot(dataframe.actors.population, aes(x = actor_g
        y = "Bevölkerung am Ereignisort") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-print(boxplot.actors.population)
--- graphs code/association_pop_deaths.R
+++ graphs code/association_pop_deaths.R
@@ -1,16 +1,13 @@
-
-dataframe_population_deaths_correlation <- subset(nigeria.merged, population_best > 0 & fatalities >= 0) #Nur Datensätze wo die untersuchten Daten vorhanden sind
+#Nur Datensätze wo die untersuchten Daten vorhanden sind
+dataframe.population.deaths.correlation <- subset(nigeria.merged, population_best > 0 & fatalities >= 0) 
 
 # Scatterplot
-plot_population_deaths_correlation <- ggplot(dataframe_population_deaths_correlation, aes(x = population_best, y = fatalities)) +
+ggplot(dataframe.population.deaths.correlation, aes(x = population_best, y = fatalities)) +
   geom_point(alpha = 0.5, color = "blue") + 
   scale_x_continuous(labels = scales::comma) +
   labs(
-    title = "Zusammenhang zwischen Bevölkerungszahl und Todesfälle",
+    title = "Zusammenhang zwischen Bevölkerungszahl und Todesfällen",
     x = "Bevölkerungszahl",
     y = "Anzahl der Todesopfer"
   ) +
   theme_minimal()
-
-
-print(plot)
