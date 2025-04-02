07a3aecc8cd43410a863c74ffdb7a5aa26fd4739
FabBob <B.Faber@campus.lmu.de>
2025-04-02 16:20:05 +0200

    renaming, neues bild, variablen umbenennnen

--- graphs code/actors_pop_distr.R
+++ graphs code/actors_pop_distr.R
@@ -1,17 +1,17 @@
 
-dataframe_acteurs_population <- nigeria.merged
+dataframe.actors.population <- nigeria.merged
 
 
-dataframe_acteurs_population <- dataframe_acteurs_population %>% filter(!is.na(population_best))
+dataframe.actors.population <- dataframe.actors.population %>% filter(!is.na(population_best))
 
 
-boxplot_acteurs_population <- ggplot(dataframe_acteurs_population, aes(x = actor_group1, y = population_best)) +
-  geom_boxplot(outlier.shape = NA, fill = "steelblue", alpha = 0.6) +
+boxplot.actors.population <- ggplot(dataframe.actors.population, aes(x = actor_group1, y = population_best)) +
+  geom_boxplot(outlier.shape = NA, fill = "#779776", alpha = 0.6) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 125000)) + 
-  labs(title = "Bevölkerungsanzahl an Ereignisorten der Akteursgruppen",
+  labs(title = "Zusammenhang zwischen Akteursgruppen und der Bevölkerungsanzahl am Ereignisort",
        x = "Akteure",
        y = "Bevölkerung am Ereignisort") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-print(boxplot_acteurs_population)
+print(boxplot.actors.population)
