6b6620e8c4b85223f6e89d9b92dbe05665a7af16
schneider1210 <153742896+schneider1210@users.noreply.github.com>
2025-04-02 14:42:26 +0200

    Update Zusammenhang zwischen Bevölkerungszahl und Todesfälle.R

--- graphs code/Zusammenhang zwischen Bevölkerungszahl und Todesfälle.R
+++ graphs code/Zusammenhang zwischen Bevölkerungszahl und Todesfälle.R
@@ -1,8 +1,8 @@
 
-df_population <- subset(nigeria_merged, population_best > 0 & fatalities >= 0) #Nur Datensätze wo die untersuchten Daten vorhanden sind
+dataframe_population_deaths_correlation <- subset(nigeria.merged, population_best > 0 & fatalities >= 0) #Nur Datensätze wo die untersuchten Daten vorhanden sind
 
 # Scatterplot
-plot <- ggplot(df_population, aes(x = population_best, y = fatalities)) +
+plot_population_deaths_correlation <- ggplot(dataframe_population_deaths_correlation, aes(x = population_best, y = fatalities)) +
   geom_point(alpha = 0.5, color = "blue") + 
   scale_x_continuous(labels = scales::comma) +
   labs(
