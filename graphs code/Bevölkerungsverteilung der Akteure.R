f16dca0723bd7ec4b0edb192498327515f614f59
schneider1210 <153742896+schneider1210@users.noreply.github.com>
2025-04-02 14:40:30 +0200

    Update Bevölkerungsverteilung der Akteure.R

--- graphs code/Bevölkerungsverteilung der Akteure.R
+++ graphs code/Bevölkerungsverteilung der Akteure.R
@@ -1,14 +1,11 @@
-library(ggplot2)
-library(dplyr)
+
+dataframe_acteurs_population <- nigeria.merged
 
 
-df <- nigeria.merged
+dataframe_acteurs_population <- dataframe_acteurs_population %>% filter(!is.na(population_best))
 
 
-df <- df %>% filter(!is.na(population_best))
-
-
-boxplot <- ggplot(df, aes(x = actor_group1, y = population_best)) +
+boxplot_acteurs_population <- ggplot(dataframe_acteurs_population, aes(x = actor_group1, y = population_best)) +
   geom_boxplot(outlier.shape = NA, fill = "steelblue", alpha = 0.6) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 125000)) + 
   labs(title = "Bevölkerungsanzahl an Ereignisorten der Akteursgruppen",
@@ -17,4 +14,4 @@ boxplot <- ggplot(df, aes(x = actor_group1, y = population_best)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-print(boxplot)
+print(boxplot_acteurs_population)
