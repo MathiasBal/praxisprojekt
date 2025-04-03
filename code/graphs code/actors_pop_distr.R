2927b070073a0ed1b0d853c7b4889e634f90384e
FabBob <B.Faber@campus.lmu.de>
2025-04-03 17:04:13 +0200

    adrians beschriftung wieder und unnötigen code gelöscht

--- code/graphs code/actors_pop_distr.R
+++ code/graphs code/actors_pop_distr.R
@@ -1,22 +1,22 @@
+#association between actors and population size
 
-dataframe.actors.population <- nigeria.merged
-
-
-dataframe.actors.population <- dataframe.actors.population %>% filter(!is.na(population_best)) %>%
-  
-mutate(
-  actor_group1 = case_when(
-    str_detect(actor_group1, "State Forces") ~ "Staatliche Sicherheitskräfte",
-    str_detect(actor_group1, "Rebel Groups") ~ "Rebellengruppen",
-    str_detect(actor_group1, "Political Militias") ~ "Politische Milizen",
-    str_detect(actor_group1,"Identity Militias" ) ~ "Identitätsmilizen",
-    str_detect(actor_group1, "Rioters") ~ "Aufständische",
-    str_detect(actor_group1, "Protesters") ~ "Protestierende",
-    str_detect(actor_group1, "Civilians") ~ "Zivilsten",
-    str_detect(actor_group1, "External/Other Forces") ~"Externe/Sontsige Akteure")
+#removing nas for population size and translating actor groups
+dataframe.actors.population <- nigeria.merged %>% 
+  filter(!is.na(population_best)) %>%
+  mutate(
+    actor_group1 = case_when(
+      str_detect(actor_group1, "State Forces") ~ "Staatliche Sicherheitskräfte",
+      str_detect(actor_group1, "Rebel Groups") ~ "Rebellengruppen",
+      str_detect(actor_group1, "Political Militias") ~ "Politische Milizen",
+      str_detect(actor_group1,"Identity Militias" ) ~ "Identitätsmilizen",
+      str_detect(actor_group1, "Rioters") ~ "Aufständische",
+      str_detect(actor_group1, "Protesters") ~ "Protestierende",
+      str_detect(actor_group1, "Civilians") ~ "Zivilsten",
+      str_detect(actor_group1, "External/Other Forces") ~"Externe/Sontsige Akteure")
   )
 
 
+# boxplot
 ggplot(dataframe.actors.population, aes(x = actor_group1, y = population_best)) +
   geom_boxplot(outlier.shape = NA, fill = "#779776", alpha = 0.6) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 125000)) + 
